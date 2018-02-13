3af3fd6deb23bc1e37ec71a67d2103859d955ebd
anton0609 <36210871+anton0609@users.noreply.github.com>
2018-02-13 15:07:54 +0100

    Added {}

--- Customer.java
+++ Customer.java
@@ -4,17 +4,27 @@ public class Customer{
   * Skapar en kund (kontoinnehavare) med namnet ’name’ och id-nummer ’idNr’.
   * Kunden tilldelas också ett unikt kundnummer.
   */
-  Customer(String name, long idNr);
+  Customer(String name, long idNr) {
+    
+  }
   
   /** Tar reda på kundens namn. */
-  String getName();
+  String getName(){
+    
+  }
   
   /** Tar reda på kundens personnummer. */
-  long getIdNr();
+  long getIdNr(){
+    
+  }
   
   /** Tar reda på kundens kundnummer. */
-  int getCustomerNr();
+  int getCustomerNr(){
+    
+  }
   
   /** Returnerar en strängbeskrivning av kunden. */
-  String toString();
+  String toString(){
+    
   }
+ }
